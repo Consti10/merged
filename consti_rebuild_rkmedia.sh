3977745ec277c91713ea5f76ce7177aaca9288b6
consti10 <geierconstantinabc@gmail.com>
2021-09-13 23:50:09 +0200

    flash every rebuild

--- consti_rebuild_rkmedia.sh
+++ consti_rebuild_rkmedia.sh
@@ -11,3 +11,9 @@ rm -rf buildroot/output/rockchip_rv1126_rv1109/build/rkmedia
 echo "removed directory"
 
 ./build.sh 
+
+echo "rebuild done"
+
+adb push buildroot/output/rockchip_rv1126_rv1109/build/rkmedia/examples/consti_test /oem/usr/bin/consti_test
+
+echo "flashing done"
