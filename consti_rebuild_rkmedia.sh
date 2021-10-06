6fba126ca6ab9563d47f33b8f1d94ce7249397e2
consti10 <geierconstantinabc@gmail.com>
2021-10-06 16:48:57 +0200

    also push test program

--- consti_rebuild_rkmedia.sh
+++ consti_rebuild_rkmedia.sh
@@ -15,5 +15,6 @@ echo "removed directory"
 echo "rebuild done"
 
 adb push buildroot/output/rockchip_rv1126_rv1109/build/rkmedia/examples/consti_test /oem/usr/bin/consti_test
+adb push buildroot/output/rockchip_rv1126_rv1109/build/rkmedia/examples/consti_run_isp /oem/usr/bin/consti_run_isp
 
 echo "flashing done"
